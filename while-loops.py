41ef59c20f2ae080a273a0c13e9f196576fe73b9
mwangisam203 <mwangisam203@gmail.com>
2026-01-06 13:54:23 -0500

    while-loop codes

--- while-loops.py
+++ while-loops.py
@@ -2,3 +2,44 @@ i = 0
 while i < 5:
     print(i)
     i += 1
+
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1 # Increment i to avoid infinite loop
+            #output: apple, banana, cherry
+
+
+# break in a while loop
+fruits = ["apple", "banana", "cherry"]  
+i = 0
+while i < len(fruits):
+    if fruits[i] == "banana":
+        break
+    print(fruits[i])
+    i += 1 #output: apple
+
+# continue in a while loop
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+i = 0
+while i < len(fruits):
+    if fruits[i] == "banana" or fruits[i] == "orange":
+        i += 1
+        continue
+    print(fruits[i])
+    i += 1
+#output: apple, cherry, kiwi, melon, mango
+
+#or
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+i = 0
+while i < len(fruits):
+    if fruits[i] == "orange":
+        i += 1
+        continue
+    print(fruits[i])
+    i += 1
+#output: apple, banana, cherry, kiwi, melon, mango
+
